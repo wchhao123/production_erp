b3e0ea93304c3a18ffebfde8ba6ec718846c7678
wyl <402261778@qq.com>
2019-08-10 00:01:20 +0800

    custom增删改查 v0.1

--- src/main/java/com/team/controller/CustomController.java
+++ src/main/java/com/team/controller/CustomController.java
@@ -1,13 +1,20 @@
 package com.team.controller;
 
+import com.team.bean.COrder;
 import com.team.bean.Custom;
+import com.team.bean.Product;
 import com.team.bean.ResponseOV;
 import com.team.service.ICustomService;
+import com.team.util.ControllerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("custom")
@@ -37,5 +44,69 @@ public class CustomController {
     @ResponseBody
     public List<Custom> getData() {
         return customService.getCustoms();
+    }
+
+    @GetMapping("edit_judge")
+    @ResponseBody
+    public void editJudge() {
+
+    }
+
+    @GetMapping("edit")
+    public String editPage() {
+        return "/WEB-INF/jsp/order_edit.jsp";
+    }
+
+    @PostMapping("update_all")
+    @ResponseBody
+    public Map<String, String> updateAll(Custom custom) {
+
+        boolean b = customService.updateCustom(custom);
+        return ControllerUtil.returnMsg(b);
+    }
+
+    @GetMapping("delete_judge")
+    @ResponseBody
+    public void deleteJudge() {
+
+    }
+
+    @PostMapping("delete_batch")
+    @ResponseBody
+    public Map<String, String> deleteBatch(String[] ids) {
+        boolean b = customService.deleteByIds(ids);
+        return ControllerUtil.returnMsg(b);
+    }
+
+    @GetMapping("add_judge")
+    @ResponseBody
+    public String insertJudge() {
+        return "";
+    }
+
+    @GetMapping("add")
+    public String addPage() {
+        return "/WEB-INF/jsp/custom_add.jsp";
+
+    }
+
+    @PostMapping("insert")
+    @ResponseBody
+    public Map<String, String> insert(Custom custom) {
+        boolean b = customService.insertCustom(custom);
+        return ControllerUtil.returnMsg(b);
+    }
+
+    @GetMapping(value = {"search_custom_by_customId","search_custom_by_customName"})
+    @ResponseBody
+    public ResponseOV<Custom> searchOrderById(HttpServletRequest request,
+                                              String searchValue, int page, int rows) {
+        int flag;
+        if (request.getRequestURI().endsWith("search_custom_by_customId")) {
+            flag = 1;
+        } else {
+            flag= 2;
+        }
+        return customService.searchCustomByCondition(flag,searchValue, page, rows);
     }
 }
