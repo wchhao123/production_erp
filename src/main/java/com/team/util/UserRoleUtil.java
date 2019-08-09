89eccf26ae96aaa86551ae5b72d4747c32b6ffad
Loong1512 <745805616@qq.com>
2019-08-09 20:16:32 +0800

    质量监控list

--- src/main/java/com/team/util/UserRoleUtil.java
+++ src/main/java/com/team/util/UserRoleUtil.java
@@ -1,6 +1,5 @@
 package com.team.util;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,9 +11,23 @@ public class UserRoleUtil {
         if (lock == 1) {
             map.put("rolename", "计划进度管理员");
             planManger(list);
+        } else if (lock == 5) {
+            map.put("rolename", "质量监控管理员");
+            qualifyMonitor(list);
         }
 
     }
+
+    private static void qualifyMonitor(List<String> list) {
+        for (String s: author ) {
+            list.add("unqualify" + s);
+            list.add("fMeasureCheck" + s);
+            list.add("fCountCheck" + s);
+            list.add("pMeasureCheck" + s);
+            list.add("pCountCheck" + s);
+        }
+    }
+
 
     private static void planManger(List<String> list) {
         for (String s: author ) {
