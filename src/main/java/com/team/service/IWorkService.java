5fb44285786c396c5c8b5ef42b0d49754fb63a0e
wyl <402261778@qq.com>
2019-08-10 14:59:52 +0800

    work v0.2

---
+++ src/main/java/com/team/service/IWorkService.java
@@ -0,0 +1,16 @@
+package com.team.service;
+
+import com.team.bean.ResponseOV;
+import com.team.bean.Work;
+
+public interface IWorkService {
+    ResponseOV<Work> getWorks(int page, int rows);
+
+    boolean updateWork(Work work);
+
+    boolean deleteWorksByIds(String[] ids);
+
+    boolean insertWork(Work work);
+
+    ResponseOV<Work> searchWorkByCondition(int flag, String searchValue, int page, int rows);
+}
