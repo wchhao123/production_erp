71f8354b1204892e395db548cb5e826e70e5a529
Loong1512 <745805616@qq.com>
2019-08-10 22:23:25 +0800

    工序计量质检和工序计数质检全部功能

--- src/main/java/com/team/service/ProcessMeasureCheckServiceImpl.java
+++ src/main/java/com/team/service/ProcessMeasureCheckServiceImpl.java
@@ -1,11 +1,16 @@
 package com.team.service;
 
 import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.team.bean.ProcessMeasureCheck;
+import com.team.bean.ProcessMeasureCheckExample;
+import com.team.bean.ResponseOV;
 import com.team.mapper.ProcessMeasureCheckMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -17,5 +22,37 @@ public class ProcessMeasureCheckServiceImpl implements ProcessMeasureCheckServic
         PageHelper.startPage(page, rows);
         List<ProcessMeasureCheck> processMeasureChecks = processMeasureCheckMapper.getPageProcessMeasureCheck();
         return processMeasureChecks;
+    }
+
+    @Override
+    public boolean insertProcessMeasureCheck(ProcessMeasureCheck processMeasureCheck) {
+        int insert = processMeasureCheckMapper.insert(processMeasureCheck);
+        return insert == 1;
+    }
+
+    @Override
+    public boolean updateProcessMeasureCheck(ProcessMeasureCheck processMeasureCheck) {
+        int update = processMeasureCheckMapper.updateByPrimaryKeySelective(processMeasureCheck);
+        return update == 1;
+    }
+
+    @Override
+    public boolean deleteByIds(String[] ids) {
+        ProcessMeasureCheckExample example = new ProcessMeasureCheckExample();
+        example.createCriteria().andPMeasureCheckIdIn(new ArrayList<>(Arrays.asList(ids)));
+        int i = processMeasureCheckMapper.deleteByExample(example);
+        return i != 0;
+    }
+
+    @Override
+    public ResponseOV<ProcessMeasureCheck> searchProcessMeasureCheckByCondition(String searchValue, int page, int rows) {
+        PageHelper.startPage(page, rows);
+        List<ProcessMeasureCheck> processMeasureChecks = processMeasureCheckMapper.searchProcessMeasureCheckByCondition("%" + searchValue + "%");
+        PageInfo<ProcessMeasureCheck> pageInfo = new PageInfo<>(processMeasureChecks);
+
+        ResponseOV<ProcessMeasureCheck> responseOV = new ResponseOV<>();
+        responseOV.setTotal((int) pageInfo.getTotal());
+        responseOV.setRows(processMeasureChecks);
+        return responseOV;
     }
 }
